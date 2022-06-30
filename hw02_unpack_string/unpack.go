380a6dba9a3dc86e8d63ed0aacc175ab0b506787
Denis Dyachkov <denis.dyachkov@btb.dev>
2022-07-01 06:59:14 +0700

    base task impl

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,36 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strings"
+	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+const emptyToken = rune(0)
+
+func Unpack(in string) (string, error) {
+	var err error
+	token := emptyToken
+	builder := strings.Builder{}
+	for _, r := range []rune(in) {
+		if !unicode.IsDigit(r) {
+			if token != emptyToken {
+				builder.WriteRune(token)
+			}
+			token = r
+			continue
+		}
+		if token == emptyToken {
+			err = ErrInvalidString
+			break
+		}
+		unpacked := strings.Repeat(string(token), int(r-'0'))
+		_, err = builder.WriteString(unpacked)
+		token = emptyToken
+	}
+	if token != emptyToken {
+		builder.WriteRune(token)
+	}
+	return builder.String(), err
 }
