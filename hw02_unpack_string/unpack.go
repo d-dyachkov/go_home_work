68859e6666248c81d70db52114776f08a00839d4
Denis Dyachkov <denis.dyachkov@btb.dev>
2022-07-01 06:59:14 +0700

    add asterisk

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,36 +2,59 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strconv"
 	"strings"
 	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-const emptyToken = rune(0)
+const (
+	emptyToken rune = 0
+	slashToken rune = '\\'
+)
 
 func Unpack(in string) (string, error) {
-	var err error
-	token := emptyToken
-	builder := strings.Builder{}
-	for _, r := range []rune(in) {
-		if !unicode.IsDigit(r) {
-			if token != emptyToken {
-				builder.WriteRune(token)
+	escaped := false
+	prevToken := emptyToken
+	builder := &strings.Builder{}
+	for _, token := range in {
+		count := 1
+		switch {
+		case token == slashToken:
+			if escaped = !escaped; escaped {
+				token = emptyToken
 			}
-			token = r
-			continue
+		case escaped && unicode.IsDigit(token):
+			prevToken = emptyToken
+			escaped = false
+		case !escaped && unicode.IsDigit(token):
+			if prevToken == emptyToken {
+				return "", ErrInvalidString
+			}
+			count, _ = strconv.Atoi(string(token))
+			token = emptyToken
+		case escaped:
+			return "", ErrInvalidString
 		}
-		if token == emptyToken {
-			err = ErrInvalidString
-			break
+		if err := writeUnpacked(builder, prevToken, count); err != nil {
+			return "", err
 		}
-		unpacked := strings.Repeat(string(token), int(r-'0'))
-		_, err = builder.WriteString(unpacked)
-		token = emptyToken
+		prevToken = token
 	}
-	if token != emptyToken {
-		builder.WriteRune(token)
-	}
+	err := writeUnpacked(builder, prevToken, 1)
 	return builder.String(), err
+}
+
+func writeUnpacked(b *strings.Builder, r rune, count int) error {
+	var err error
+	if r != emptyToken {
+		if count != 1 {
+			unpacked := strings.Repeat(string(r), count)
+			_, err = b.WriteString(unpacked)
+		} else {
+			_, err = b.WriteRune(r)
+		}
+	}
+	return err
 }
